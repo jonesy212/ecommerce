cd582bea427532c0a8dc239aa7a25e615ce0c7c8
Marcus Jones <indelibleink84@gmail.com>
2022-08-09 18:42:49 -0500

    Updated buy button
    Updated button to be active on click

--- shoe-store/app.js
+++ shoe-store/app.js
@@ -127,13 +127,17 @@ currentProductSizes.forEach((size, index) => {
 });
 
 const productButton = document.querySelector(".productButton");
-const payment = document.querySelector("payment");
-const close = document.querySelector("close");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
 
 productButton.addEventListener("click", () => {
   payment.style.display = "flex";
+  if ("click" == true) {
+    productButton.style.backgroundColor = "black";
+    productButton.style.color = "white";
+  }
 });
 
-productButton.addEventListener("click", () => {
+close.addEventListener("click", () => {
   payment.style.display = "none";
 });
